f7ee4b2befc1daf6c0460f7f6fe905ca67e169a5
Jose Luis Rodriguez <josesietepicos@gmail.com>
2021-08-02 02:05:41 +0200

    config cors

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ require('dotenv').config();
 
 
 // const jwt = require("jsonwebtoken");
-// const cors = require('./config/cors.config')
+const cors = require('./config/cors.config')
 
 const mongoose = require('mongoose');
 const createError = require('http-errors');
@@ -26,17 +26,7 @@ const app = express();
 
 app.use(express.json());
 app.use(logger('dev'));
-// app.use(cors);
-
-// Configurar cabeceras y cors
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
-
+app.use(cors);
 
 // Web Title
 // app.locals.title = 'Pet&Vet';
--- config/cors.config.js
+++ config/cors.config.js
@@ -2,10 +2,7 @@ const cors = require('cors')
 
 const corsMiddleware = cors({
     origin : process.env.CORS_ORIGIN || 'http://localhost:3000',
-    // allowedHeaders: ["Content-Type", "Authorization"]
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-    "preflightContinue": false,
-    "optionsSuccessStatus": 204
+    allowedHeaders: ["Content-Type", "Authorization"]
 })
 
 module.exports = corsMiddleware
