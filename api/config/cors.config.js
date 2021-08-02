dd64f88947ec3b28db179107fb6d9d869b46064a
Jose Luis Rodriguez <josesietepicos@gmail.com>
2021-08-03 01:25:18 +0200

    config vercel

--- api/config/cors.config.js
+++ api/config/cors.config.js
@@ -1,8 +1,26 @@
 const cors = require('cors')
 
-const corsMiddleware = cors({
-    origin : process.env.CORS_ORIGIN || 'http://localhost:3000',
-    allowedHeaders: ["Content-Type", "Authorization"]
-})
+const corsMiddleware = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+      res.status(200).end()
+      return
+    }
+    return await fn(req, res)
+  }
+  
+  const handler = (req, res) => {
+      console.log(req.body)
+    const d = new Date()
+    res.end(d.toString())
+  }
 
-module.exports = corsMiddleware
+module.exports = corsMiddleware(handler)
